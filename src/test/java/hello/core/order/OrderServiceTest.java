1bdf9c4044c3a5ec81ba4828531a5e2a9a578202
xioix94 <xioix94@gmail.com>
2024-02-15 00:56:59 +0900

    init commit

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,21 @@
+package hello.core.order;
+
+import hello.core.member.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "itemA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
